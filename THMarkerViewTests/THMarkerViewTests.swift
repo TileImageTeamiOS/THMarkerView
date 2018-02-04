37d2a22bd87f445f196d3e498335f55786296065
Seong ho Hong <grohong76@gmail.com>
2018-02-04 15:37:45 +0900

    set init test

--- THMarkerViewTests/THMarkerViewTests.swift
+++ THMarkerViewTests/THMarkerViewTests.swift
@@ -7,12 +7,26 @@
 //
 
 import XCTest
+@testable import THMarkerView
 
 class THMarkerViewTests: XCTestCase {
+    var scrollView = UIScrollView()
+    var markerArray = [THMarkerView]()
+    
+    var size = CGSize(width: 20, height: 20)
+    var point = CGPoint(x:1500, y:1500)
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let marker = THMarkerView()
+        marker.frame.size = size
+        marker.set(origin: point, zoomScale: 2.0, scrollView: scrollView)
+        
+        marker.delegate = self
+        scrollView.delegate = self
+        
+        marker.index = markerArray.count
+        markerArray.append(marker)
     }
     
     override func tearDown() {
@@ -20,16 +34,24 @@ class THMarkerViewTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testSet() {
+        XCTAssertEqual(markerArray[0].frame.size, size)
+        XCTAssertEqual(markerArray[0].frame.origin.x, scrollView.zoomScale*point.x)
+        XCTAssertEqual(markerArray[0].frame.origin.y, scrollView.zoomScale*point.y)
+        XCTAssertEqual(markerArray[0].index, 0)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+}
+
+extension THMarkerViewTests: UIScrollViewDelegate {
+    public func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        markerArray.map { marker in
+            marker.framSet()
         }
     }
-    
+}
+
+extension THMarkerViewTests: THMarkerViewDelegate {
+    func tapEvent(marker: THMarkerView) {
+        
+    }
 }
